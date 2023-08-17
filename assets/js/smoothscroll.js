afed6b61876cf6788eb80ae8d44a82eab393d32a
Tim Nicholson <timrnicholson@gmail.com>
2023-08-17 10:44:10 -0500

    v1.2.14
    * Adjusted vertical margin in Header Centered Template Parts
    * Remove re-focus on Smooth Scroll javascript as it was causing issues

--- assets/js/smoothscroll.js
+++ assets/js/smoothscroll.js
@@ -33,6 +33,7 @@
 				// Figure out element to scroll to
 				var target = $(this.hash);
 				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				//console.log('target id=' + target.id); //TEST
 
 				// Does a scroll target exist?
 				if (target.length) {
@@ -43,6 +44,7 @@
 					if (siteTopMargin) topOffset += siteTopMargin;
 					const adminbarHeight = parseInt( $( '#wpadminbar' ).outerHeight() );
 					if (adminbarHeight) topOffset += adminbarHeight;
+					//console.log('topOffset=' + topOffset); //TEST
 
 					// Only prevent default if animation is actually gonna happen
 					event.preventDefault();
@@ -51,14 +53,14 @@
 					}, 1000, function() {
 						// Callback after animation
 						// Must change focus!
-						var $target = $(target);
+						/*var $target = $(target);
 						$target.focus();
 						if ($target.is(":focus")) { // Checking if the target was focused
 							return false;
 						} else {
 							$target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
 							$target.focus(); // Set focus again
-						};
+						};*/
 					});
 				} // target
 			} // On-page links
