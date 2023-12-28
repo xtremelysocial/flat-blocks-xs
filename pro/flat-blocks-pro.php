0ae3365407a37f47eaf6a0e21292f974daa8d8ff
Tim Nicholson <timrnicholson@gmail.com>
2023-12-28 14:34:41 -0600

    Update flat-blocks-pro.php
    * Remove commented out code for Admin Edit Link now that it's been moved to its own include file

--- pro/flat-blocks-pro.php
+++ pro/flat-blocks-pro.php
@@ -56,25 +56,3 @@ if ( apply_filters( 'flatblocks_pro_load_woocommerce', $default = true ) ) {
 		require_once get_template_directory() . '/pro/inc/pro-woocommerce.php';
 	}
 }
-
-/**
- * Admin Edit Link on Pages
- *
- * Adds an edit post/page link only for site admins
- * 
-*/
-/*if ( ! function_exists( 'flatblocks_pro_add_edit_link' ) ) :
-	function flatblocks_pro_add_edit_link ( $content ) {
-		$post_id = get_the_ID();
-		if( is_singular() and current_user_can( 'edit_post', $post_id ) ) {
-			$content = $content . '<p class="edit-post-link has-small-font-size has-text-align-center dashicons dashicons-edit" style="display: block"><a href="' 
-				. get_edit_post_link( $post_id ) 
-				. '">' . __("Edit", "flat-blocks") 
-				. '</a></p>';
-		}
-		return $content;	
-	}
-endif;
-if ( apply_filters( 'flatblocks_pro_add_edit_link', $default = true ) ) {
-	add_filter('the_content', 'flatblocks_pro_add_edit_link');
-}*/
